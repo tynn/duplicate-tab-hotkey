d17ce41666630c15b3679137ef652d2114576b0a
tynn <tynn.dev@gmail.com>
2018-02-17 11:16:35 +0100

    Port Add-on to WebExtensions

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ The files of this addon are licensed under the
 [MPL 2.0](http://mozilla.org/MPL/2.0/).
 
 If enabled, a keyboard shortcut for duplicating
-the current tab will be bound to `Accel+K`.
-Since this conflicts with one of the shortcuts
-for the Search Bar, an alternate shortcut like
-`Ctrl+J` should be used for this purpose.
+the current tab will be bound to `Accel+Shift+K`.
+Since `Accel+K` conflicts with one of the shortcuts
+for the Search Bar, which has precedence over custom
+shortcuts, it cannot be used anymore.
 
--- accel-k.js
+++ accel-k.js
@@ -1,20 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-"use strict";
-
-const windows = require("sdk/window/utils");
-const tabs = require("sdk/tabs/utils");
-const prefs = require("sdk/preferences/service");
-
-require("sdk/hotkeys").Hotkey({
-	combo: "accel-k",
-	onPress: function () {
-		let tab = tabs.getActiveTab(windows.getMostRecentBrowserWindow());
-		let tabbrowser = tabs.getTabBrowserForTab(tab);
-		tab = tabbrowser.duplicateTab(tab);
-		if (!prefs.get("browser.tabs.loadInBackground", true))
-			tabbrowser.selectedTab = tab;
-	}
-});
-
+browser.commands.onCommand.addListener(command => {
+  if (command == "accel-k") browser.tabs
+        .query({ currentWindow: true, active: true })
+        .then(tabs => browser.tabs.duplicate(tabs[0].id))
+})
